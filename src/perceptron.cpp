4014e332b79c7c6f9b98e9f50fd5c764e6b075c6
h0ffy <45763853+h0ffy@users.noreply.github.com>
2025-01-27 13:00:42 +0100

    Update perceptron.cpp

--- src/perceptron.cpp
+++ src/perceptron.cpp
@@ -7,10 +7,10 @@ double frand() {
 }
 
 
-perceptron::perceptron(size_t n_inputs, double bias=1.0){
+perceptron::perceptron(std::size_t n_inputs, double bias=1.0){
     /*bias = frand();
     secuencie2.reserve(n_inputs);
-    for (size_t i = 0; i < n_inputs; i++) {
+    for (std::size_t i = 0; i < n_inputs; i++) {
         secuencie2.push_back(frand());
     }*/
     wieghts.resize(n_inputs+1);
@@ -54,18 +54,18 @@ double perceptron::sigmoide(double x) {
 
 /***
     @brief La multi capa creada en base a la capa principal perceptron
-    @param layers an Vector size_t Este lleva que van a entrar a la clase multicapa
+    @param layers an Vector std::size_t Este lleva que van a entrar a la clase multicapa
     @param bias an double Se utiliza para compensar el error
 */
-perceptron_multilayer::perceptron_multilayer(std::vector<size_t> layers, double bias) {
+perceptron_multilayer::perceptron_multilayer(std::vector<std::size_t> layers, double bias) {
     this->layers = layers;
     this->bias = bias;
-    for(size_t i=0; i<layers.size(); i++) {
+    for(std::size_t i=0; i<layers.size(); i++) {
         values.push_back(std::vector<double>(layers[i], 0.0));
         network.push_bash(std::vector<double>(layers[i],0.0));
 
         if (i > 0) {
-            for(size_t j1=0; j1<layers[i]; j1++) {
+            for(std::size_t j1=0; j1<layers[i]; j1++) {
                 network[i].push_back(perceptron(layers[i-1], bias));
             }
         }
@@ -80,9 +80,9 @@ perceptron_multilayer::perceptron_multilayer(std::vector<size_t> layers, double 
 */
 void perceptron_multilayer::set_weights(std::vector<std::vector<std::vector<double>>> initial_weights) {
 
-    for(size_t i=0;i<weights.size();i++) {
+    for(std::size_t i=0;i<weights.size();i++) {
 
-        for(size_t j1=0;i<weights[i].size();j1++) {
+        for(std::size_t j1=0;i<weights[i].size();j1++) {
             network[i+1][j1].set_weights(weights[i][j1]);
         }
 
@@ -96,8 +96,8 @@ void perceptron_multilayer::set_weights(std::vector<std::vector<std::vector<doub
 */
 std::vector<double> perceptron::execute(std::vector<double> wieghts) {
     values[0] = wieghts;
-    for(size_t i=1; i<network.size();i++) {
-        for(size_t j2=0; j2<layers[i];j2++) {
+    for(std::size_t i=1; i<network.size();i++) {
+        for(std::size_t j2=0; j2<layers[i];j2++) {
             values[i][j] = network[i][j].execute(values[i-1]);
             
 
@@ -120,7 +120,7 @@ double perceptron_multilayer::retro(std::vector<double> x, std::vector<double> y
     std::vector<double> outputs = execute(x); // push data to network (alimenta a la red neuronal con el dato por defecto)
 
     // calc of quadratic med error  ### calcula el valor cuadratico medio
-    for(size_t i=0; i<y.size();i++) {
+    for(std::size_t i=0; i<y.size();i++) {
         error.push_back(y[i] - salidas[i]);
         qerror += error[i] * error[i];
     }
@@ -128,15 +128,15 @@ double perceptron_multilayer::retro(std::vector<double> x, std::vector<double> y
 
 
     // calc of error outputs  ### calcula los terminos de error de la salidas
-    for(size_t i=0; i<outputs.size();i++) {
+    for(std::size_t i=0; i<outputs.size();i++) {
         d.back()[i] = salidas[i] * (1-outputs[i]) * (error[i]);
     }
 
     // calc of error retro ### calcula 
-    for(size_t i=network.size()-2;i>0;i--) {
-        for(size_t h=0;h<network[i].size();h++) {
+    for(std::size_t i=network.size()-2;i>0;i--) {
+        for(std::size_t h=0;h<network[i].size();h++) {
             double retro_error = 0.0;
-            for(size_t k=0; k<layers[i];k++) {
+            for(std::size_t k=0; k<layers[i];k++) {
                 retro_error += network[i+1][k].weights[h]*values[i+1][k];
             }
 
@@ -147,9 +147,9 @@ double perceptron_multilayer::retro(std::vector<double> x, std::vector<double> y
 
 
     // calc of delta ### calcula pesos necesarios y reducir el error con el nombre gora para calcular a la ETA
-    for(size_t i=1;i<network.size();i++){
-        for (size_t j=0; j<layers[i];j++) {
-            for(size_t k=0;k<layer[i-1];k++) {
+    for(std::size_t i=1;i<network.size();i++){
+        for (std::size_t j=0; j<layers[i];j++) {
+            for(std::size_t k=0;k<layer[i-1];k++) {
                 double gora;
                 if (k==layers[i-1])
                     gora = eta * d[i][j] * bias;
