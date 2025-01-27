0da124101d5ee04d83a8ed0f759278f891364d52
h0ffy <45763853+h0ffy@users.noreply.github.com>
2025-01-27 15:26:38 +0100

    Update perceptron.h

--- src/perceptron.h
+++ src/perceptron.h
@@ -1,21 +1,22 @@
-#include <iostream> 
+#include <iostream>
 #include <cstdlib>
 #include <string>
 #include <ctime>
 #include <vector>
 #include <numeric>
+#include <cstddef>  // Add this for size_t
 
 class perceptron {
 public:
-    perceptron::perceptron(std::size_t n_inputs, double bias = 1.0); // Remove extra qualification
-    
+    perceptron(std::size_t n_inputs, double bias = 1.0); // Remove extra qualification
+    ~perceptron();
     double execute(std::vector<double> inputs_data);
     void set_weights(std::vector<double> initial_weights);
     double activation_function(double x);
     double sigmoide(double x);
     std::vector<double> weights;
     double bias;
-}; 
+};
 
 class perceptron_multilayer {
 public:
@@ -24,8 +25,9 @@ public:
     std::vector<std::vector<double>> values;        //  Replicar estructura de network
     std::vector<std::vector<double>> d;
     double bias;
-    perceptron_multilayer::perceptron_multilayer(std::vector<size_t> layers, double bias); // Add constructor definition
+    perceptron_multilayer(std::vector<size_t> layers, double bias); // Add constructor definition
+    ~perceptron_multilayer();
     double retro(std::vector<double> x, std::vector<double> y);
     std::vector<double> execute(std::vector<double> weights); // Fix function signature
     void set_weights(std::vector<std::vector<std::vector<double>>> initial_weights);
-}; 
+};
