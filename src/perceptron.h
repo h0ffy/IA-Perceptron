8ae80bb30956e2ef70740e61f1f049cbf4c7955e
h0ffy <45763853+h0ffy@users.noreply.github.com>
2025-01-27 12:53:57 +0100

    Update perceptron.h

--- src/perceptron.h
+++ src/perceptron.h
@@ -1,31 +1,26 @@
 #include <vector>
 #include <numeric>
 
-
 class perceptron {
 public:
-    perceptron::perceptron(size_t n_inputs, double bias=1.0);
+    perceptron(size_t n_inputs, double bias = 1.0); // Remove extra qualification
     double execute(std::vector<double> inputs_data);
-    void set_weights(std::vector<double> inital_weights);
+    void set_weights(std::vector<double> initial_weights);
     double activation_function(double x);
     double sigmoide(double x);
     std::vector<double> weights;
     double bias;
-
-)
+}; 
 
 class perceptron_multilayer {
-    public:
-        std::vector<std::vector<perceptron>> network;   //  Estructura de network
-        std::vector<size_t> layers;                     //  Capa
-        std::vector<std::vector<double>> values;        //  Replicar estructura de network
-        std::vector<std::vector<double>> d;
-        double bias;
-        // perceptron_multilayer(std::vector<size_t> layers);
-
-        double perceptron_multilayer::retro(std::vector<double> x, std::vector<double> y)
-        std::vector<double> perceptron::execute(std::vector<double> wieghts)
-        void perceptron_multilayer::set_weights(std::vector<std::vector<std::vector<double>>> initial_weights)
-        perceptron_multilayer::perceptron_multilayer(std::vector<size_t> layers, double bias)   
-
-}
+public:
+    std::vector<std::vector<perceptron>> network;   //  Estructura de network
+    std::vector<size_t> layers;                     //  Capa
+    std::vector<std::vector<double>> values;        //  Replicar estructura de network
+    std::vector<std::vector<double>> d;
+    double bias;
+    perceptron_multilayer(std::vector<size_t> layers, double bias); // Add constructor definition
+    double retro(std::vector<double> x, std::vector<double> y);
+    std::vector<double> execute(std::vector<double> weights); // Fix function signature
+    void set_weights(std::vector<std::vector<std::vector<double>>> initial_weights);
+}; 
